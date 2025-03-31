f83d284309ad1e6fe243e7ea3df679b92f1d70ea
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-03-31 21:19:21 +0530

    Create a generateLogin function to generate the login URL with redirect to the Neeto product

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { getLoginUri, getRedirectUri, SearchParams } from "./utils.js";
 
 interface Options {
   email: string;
@@ -27,21 +28,32 @@ class NeetoJWT {
     this.privateKey = privateKey;
   }
 
-  generate() {
+  generateJWT = () => {
     const iat = Math.floor(Date.now() / 1000); // Current date in seconds.
     const exp = iat + 2 * 60; // Expiry 2 minutes from now.
 
     const payload = {
       email: this.email,
-      iss: this.workspace,
+      workspace: this.workspace,
       iat,
       exp,
     };
 
     const token = jwt.sign(payload, this.privateKey, { algorithm: "ES256" });
-
     return token;
-  }
+  };
+
+  generateLoginUrl = (product?: string) => {
+    const searchParams: SearchParams = {
+      jwt: this.generateJWT(),
+    };
+
+    if (product) {
+      searchParams.redirect_uri = getRedirectUri(this.workspace, product);
+    }
+
+    return getLoginUri(this.workspace, searchParams);
+  };
 }
 
 export default NeetoJWT;
