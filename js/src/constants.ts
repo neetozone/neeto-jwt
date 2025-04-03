58b3e0cc8229c11abc1f8777284fdb1caa26621b
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-04-03 07:50:02 +0530

    Added logic to generate client_app_name from redirect_uri

--- js/src/constants.ts
+++ js/src/constants.ts
@@ -3,3 +3,32 @@ export const TLD: Record<string, string> = {
   development: ".lvh.me:9000",
   production: "neetoauth.com",
 };
+
+export const NEETO_URL_COMPONENT_REGEX = /neeto(\w+)/;
+
+export const CLIENT_APPS = {
+  cal: "Cal",
+  form: "Form",
+  record: "Record",
+  invoice: "Invoice",
+  desk: "Desk",
+  chat: "Chat",
+  kb: "KB",
+  site: "Site",
+  runner: "Runner",
+  wireframe: "Wireframe",
+  engage: "Engage",
+  quiz: "Quiz",
+  replay: "Replay",
+  planner: "Planner",
+  crm: "CRM",
+  publish: "Publish",
+  course: "Course",
+  testify: "Testify",
+  code: "Code",
+  git: "Git",
+  ci: "CI",
+  deploy: "Deploy",
+  playdash: "Playdash",
+  tower: "Tower",
+};
--- js/src/utils.ts
+++ js/src/utils.ts
@@ -1,10 +1,9 @@
-import { TLD } from "./constants.js";
+import { CLIENT_APPS, NEETO_URL_COMPONENT_REGEX, TLD } from "./constants.js";
 
 export type SearchParams = {
   jwt: string;
-  redirect_uri?: string;
-  state: string;
-  client_app_name?: string;
+  redirect_uri: string;
+  client_app_name: string;
 };
 
 export const getLoginUri = (workspace: string, searchParams: SearchParams) => {
@@ -18,4 +17,15 @@ export const getLoginUri = (workspace: string, searchParams: SearchParams) => {
 export const getTopLevelDomain = () => {
   const env: string = process.env.NEETO_JWT_ENV || "production";
   return TLD[env] || ".com";
+};
+
+type App = keyof typeof CLIENT_APPS;
+export const getClientAppName = (redirectUri: string) => {
+  const app = redirectUri.match(NEETO_URL_COMPONENT_REGEX)?.[1];
+
+  if (app && app in CLIENT_APPS) {
+    return CLIENT_APPS[app as App];
+  }
+
+  return "Cal";
 };
